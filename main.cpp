8aba5b80d17155d1b829ba643e7f3d71636cd6b3
Simple-codinger <vincentgierisch@gmail.com>
2018-11-09 00:43:30 +0100

    init

---
+++ main.cpp
@@ -0,0 +1,8 @@
+//
+//  main.cpp
+//  
+//
+//  Created by Vincent Gierisch on 09.11.18.
+//
+
+#include "main.hpp"
---
+++ main.hpp
@@ -0,0 +1,13 @@
+//
+//  main.hpp
+//  
+//
+//  Created by Vincent Gierisch on 09.11.18.
+//
+
+#ifndef main_hpp
+#define main_hpp
+
+#include <stdio.h>
+
+#endif /* main_hpp */
