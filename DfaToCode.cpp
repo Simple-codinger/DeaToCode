52453ec3ffa096ccbcc7306682d8287ffd8cf3fe
Simple-codinger <vincentgierisch@gmail.com>
2018-11-09 02:41:35 +0100

    add DfaToCode class

---
+++ DfaToCode.cpp
@@ -0,0 +1,8 @@
+//
+//  DfaToCode.cpp
+//  
+//
+//  Created by Vincent Gierisch on 09.11.18.
+//
+
+#include "DfaToCode.hpp"
---
+++ DfaToCode.hpp
@@ -0,0 +1,13 @@
+//
+//  DfaToCode.hpp
+//  
+//
+//  Created by Vincent Gierisch on 09.11.18.
+//
+
+#ifndef DfaToCode_hpp
+#define DfaToCode_hpp
+
+#include <stdio.h>
+
+#endif /* DfaToCode_hpp */
