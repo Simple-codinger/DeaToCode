6d1d8dc1dc20cbbda5745d77b4081bceac3e21e3
Simple-codinger <vincentgierisch@gmail.com>
2018-11-09 02:40:02 +0100

    add dfa class

---
+++ Dfa.cpp
@@ -0,0 +1,8 @@
+//
+//  Dfa.cpp
+//  
+//
+//  Created by Vincent Gierisch on 09.11.18.
+//
+
+#include "Dfa.hpp"
---
+++ Dfa.hpp
@@ -0,0 +1,13 @@
+//
+//  Dfa.hpp
+//  
+//
+//  Created by Vincent Gierisch on 09.11.18.
+//
+
+#ifndef Dfa_hpp
+#define Dfa_hpp
+
+#include <stdio.h>
+
+#endif /* Dfa_hpp */
